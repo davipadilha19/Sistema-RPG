5b5c8c44454a4ed09350dac9ba581b5e182b01d9
davipadilha19 <124940713+davipadilha19@users.noreply.github.com>
2024-04-05 21:47:44 -0300

    Add files via upload

---
+++ scriptCombate.js
@@ -0,0 +1,9 @@
+let divOpcoesCombate = document.getElementById("div-opcoesCombate");
+
+function opcoesCombate() {
+  if (divOpcoesCombate.style.visibility === "visible") {
+    divOpcoesCombate.style.visibility = "hidden"; 
+  } else {
+    divOpcoesCombate.style.visibility = "visible"; 
+  }
+}
