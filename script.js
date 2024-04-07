83b43467147730f4e1f724f586a1023e0b07f77b
davipadilha19 <124940713+davipadilha19@users.noreply.github.com>
2024-04-06 21:03:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let divOpcoesUsuario = document.getElementById("div-opcoesUsuario");
 
 function opcoesUsuario() {
   if (divOpcoesUsuario.style.visibility === "visible") {
-    divOpcoesUsuario.style.visibility = "hidden"; 
+    divOpcoesUsuario.style.visibility = "hidden";
   } else {
-    divOpcoesUsuario.style.visibility = "visible"; 
+    divOpcoesUsuario.style.visibility = "visible";
   }
 }
--- scriptCombate.js
+++ scriptCombate.js
@@ -2,8 +2,8 @@ let divOpcoesCombate = document.getElementById("div-opcoesCombate");
 
 function opcoesCombate() {
   if (divOpcoesCombate.style.visibility === "visible") {
-    divOpcoesCombate.style.visibility = "hidden"; 
+    divOpcoesCombate.style.visibility = "hidden";
   } else {
-    divOpcoesCombate.style.visibility = "visible"; 
+    divOpcoesCombate.style.visibility = "visible";
   }
 }
