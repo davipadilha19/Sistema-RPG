1ee20471fbd0b2ea7720de159a120d9087aa215e
davipadilha19 <124940713+davipadilha19@users.noreply.github.com>
2024-04-10 21:47:38 -0300

    Add files via upload

--- scriptCombate.js
+++ scriptCombate.js
@@ -1,18 +1,46 @@
 let divOpcoesCombate = document.getElementById("div-opcoesCombate");
-let visualizarFicha = document.getElementById("div-visualizarPersonagem")
+let visualizarFicha = document.getElementById("div-visualizarPersonagem");
+let visualizarCriatura = document.getElementById("div-visualizarCriatura");
+let rolagemDeDados = document.getElementById("div-rolagemDeDados");
 
 function opcoesCombate() {
   if (divOpcoesCombate.style.visibility === "visible") {
     divOpcoesCombate.style.visibility = "hidden";
   } else {
     divOpcoesCombate.style.visibility = "visible";
+    visualizarFicha.style.visibility = "hidden";
+    visualizarCriatura.style.visibility = "hidden";
+    rolagemDeDados.style.visibility = "hidden";
   }
 }
 
-function visualizarC(){
-  if(visualizarFicha.style.visibility === "visible"){
-    visualizarFicha.style.visibility = "hidden"
-  } else{
-    visualizarFicha.style.visibility = "visible"
+function visualizarC() {
+  if (visualizarFicha.style.visibility === "visible") {
+    visualizarFicha.style.visibility = "hidden";
+  } else {
+    visualizarFicha.style.visibility = "visible";
+    divOpcoesCombate.style.visibility = "hidden";
+    visualizarCriatura.style.visibility = "hidden";
+    rolagemDeDados.style.visibility = "hidden";
+  }
+}
+function visualizarI() {
+  if (visualizarCriatura.style.visibility === "visible") {
+    visualizarCriatura.style.visibility = "hidden";
+  } else {
+    visualizarCriatura.style.visibility = "visible";
+    divOpcoesCombate.style.visibility = "hidden";
+    visualizarFicha.style.visibility = "hidden";
+    rolagemDeDados.style.visibility = "hidden";
+  }
+}
+function lancarDado() {
+  if (rolagemDeDados.style.visibility === "visible") {
+    rolagemDeDados.style.visibility = "hidden";
+  } else {
+    rolagemDeDados.style.visibility = "visible";
+    divOpcoesCombate.style.visibility = "hidden";
+    visualizarFicha.style.visibility = "hidden";
+    visualizarCriatura.style.visibility = "hidden";
   }
 }
